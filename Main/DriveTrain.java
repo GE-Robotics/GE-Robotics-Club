ef63e47e2427db80dda5a5f76545a364ce4470ac
FoxInTheClouds <97125461+FoxInTheClouds@users.noreply.github.com>
2023-02-08 14:47:22 -0800

    Add files via upload
    Updated for initial wheel code

--- Main/DriveTrain.java
+++ Main/DriveTrain.java
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 
+Copyright 2022
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 associated documentation files (the "Software"), to deal in the Software without restriction,
@@ -16,7 +16,6 @@ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FO
 DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -29,97 +28,93 @@ import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
- 
- //version 0.6 12/9/22
- 
- //List of controller inputs
- //gamepad1.left_stick_x or y
- //gamepad1.right_stick_x or y 
- //gamepad1.x 
- //gamepad1.y 
- //gamepad1.b 
- //gamepad1.a 
- //gamepad1.left_bumper
- //gamepad1.right_bumper
- 
+
+//version 0.6 12/9/22
+
+//List of controller inputs
+//gamepad1.left_stick_x or y
+//gamepad1.right_stick_x or y 
+//gamepad1.x 
+//gamepad1.y 
+//gamepad1.b 
+//gamepad1.a 
+//gamepad1.left_bumper
+//gamepad1.right_bumper
+
 @TeleOp
 
 public class DriveTrain extends LinearOpMode {
     private Blinker expansionHub;
-    private DcMotor motor0;
-    private DcMotor motor1;
-    private DcMotor motor2;
-    private DcMotor motor3;
+    private DcMotor motorRearLeft;
+    private DcMotor motorRearRight;
+    private DcMotor motorFrontLeft;
+    private DcMotor motorFrontRight;
     private Servo servo0;
 
     @Override
     public void runOpMode() {
         expansionHub = hardwareMap.get(Blinker.class, "Expansion Hub 2");
-        motor0 = hardwareMap.get(DcMotor.class, "motor0");
-        motor1 = hardwareMap.get(DcMotor.class, "motor1");
-        motor2 = hardwareMap.get(DcMotor.class, "motor2");
-        motor3 = hardwareMap.get(DcMotor.class, "motor3");
+        motorRearLeft = hardwareMap.get(DcMotor.class, "motorRearLeft");
+        motorRearRight = hardwareMap.get(DcMotor.class, "motorRearRight");
+        motorFrontLeft = hardwareMap.get(DcMotor.class, "motorFrontLeft");
+        motorFrontRight = hardwareMap.get(DcMotor.class, "motorFrontRight");
         servo0 = hardwareMap.get(Servo.class, "servo0");
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
-        
+
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
-        
+
         telemetry.addData("Status", "Robot is moving");
         telemetry.update();
-        
-        // run until the end of the match (driver presses STOP)
-        while (opModeIsActive()) { 
-            //Acounting for stick drift and setting the motors to which way they go
-            if (gamepad1.left_stick_y > 0.1 || gamepad1.left_stick_y < -0.1) {
-                motor1.setPower(gamepad1.left_stick_y);
-            } else {
-                motor1.setPower(0);
-            }
-            if (gamepad1.right_stick_y > 0.1 || gamepad1.right_stick_y < -0.1) {
-                motor0.setPower(gamepad1.right_stick_y * -1);
-            } else {
-                motor0.setPower(0);
-            }  
 
-            //Lifter motor            
-            if (gamepad2.dpad_up) {
-                motor2.setPower(1);
-            } else if (gamepad2.dpad_down) {
-                motor2.setPower(-1);
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            double y; 
+            double x;
+            double rx;
+            if (gamepad1.left_stick_y < -0.1 || gamepad1.left_stick_y> 0.1){
+                y = -gamepad1.left_stick_y; // Reversed for a reason don't ask me what reason though
             } else {
-                motor2.setPower(0);
+                y = 0;
+            }
+            if (gamepad1.left_stick_x < -0.1 || gamepad1.left_stick_x> 0.1){
+                x = gamepad1.left_stick_x * 1.1; // Reversed for a reason don't ask me what reason though
+            } else {
+                x = 0;
+            }
+            if (gamepad1.right_stick_x < -0.1 || gamepad1.right_stick_x > 0.1){
+                rx = gamepad1.right_stick_x; // Reversed for a reason don't ask me what reason though
+            } else {
+                rx = 0;
             }
             
-            //Backup motor 
-            if (gamepad1.dpad_left) {   
-                motor3.setPower(1);
-            } else if (gamepad1.dpad_right) {
-                motor3.setPower(-1);
-            } else {  
-                motor3.setPower(0);
-            }
-            
-          
+
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double frontLeftPower = (y + x + rx) / denominator;
+            double backLeftPower = (y - x + rx) / denominator;
+            double frontRightPower = (y - x - rx) / denominator;
+            double backRightPower = (y + x - rx) / denominator;
+
+
             //Servo for the grabber
             if (gamepad2.b) {
                 //works from 0 to 1
                 servo0.setPosition(0.35);
-                
+
             }
             if (gamepad2.y) {
                 //works from -0 to -1
-                servo0.setPosition(-0.0);
-                
+                servo0.setPosition(0);
+
             }
-            
+
             telemetry.addData("Left Stick", gamepad1.left_stick_y);
             telemetry.addData("Right Stick", gamepad1.left_stick_x);
-            telemetry.addData("Movement motor 0", motor0.getPower());
-            telemetry.addData("Movement motor 1", motor1.getPower());
-            telemetry.addData("Lifter motor", motor2.getPower());
+            //telemetry.addData("Movement motor 0", motorRearLeft.getPower());
+            //telemetry.addData("Movement motor 1", motorRea.getPower());
+            //telemetry.addData("Lifter motor", motor2.getPower());
             telemetry.addData("Servo", servo0.getPosition());
             //telemetry.addData("Backup motor", motor3.getPower());
             telemetry.update();
